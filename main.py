0fd5125348a635405896958fc68bea34061ea90d
Nhat Doan <74613870+barrydoan@users.noreply.github.com>
2023-10-17 16:25:15 -0400

    add color

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import random
 
 correct_answer = 0
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 for i in range(10):
     x = random.randrange(20)
     y = random.randrange(20)
-    result = input(f"{x} + {y} = ")
+    result = input(f"{color.GREEN}{x} + {y} = ")
     result = int(result)
     if result == (x + y):
         correct_answer+=1
-        print("Your answer is correct!")
+        print(f"Your answer is correct!{color.END}")
     else:
-        print("Your answer is incorrect!")
+        print(f"{color.RED}Your answer is incorrect!{color.END}")
 
-print(f"You have just finished your math. This is your score: {correct_answer * 10}")
+print(f"{color.GREEN}You have just finished your math. This is your score: {correct_answer * 10}{color.END}")
