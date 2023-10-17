e539d638f7d0103da98e9fb4dec7d9c9aa3ddd4d
Nhat Doan <74613870+barrydoan@users.noreply.github.com>
2023-10-17 17:30:16 -0400

    add progress

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ def clear():
     else:
         _ = system('clear')
 
+def print_progress(done, total):
+    print("Progress: |", end="")
+    print("|", end="")
+    for i in range(done):
+        print("#####", end="")
+    for i in range(total - done):
+        print("_____", end="")
+    print("|", end="")
+    print(f" {done} / {total}")
+
+
 def print_history(history):
     for i in history:
         print(i)
@@ -18,6 +29,7 @@ change_question = True
 x = 0
 y = 0
 history = []
+progress = 0
 
 class color:
    PURPLE = '\033[95m'
@@ -34,23 +46,26 @@ class color:
 clear()
 
 for i in range(10):
+    progress +=1
+    clear()
+    print_progress(done=progress, total=10)
+    print_history(history=history)
     if change_question:
         x = random.randrange(20)
         y = random.randrange(20)
     result = input(f"{color.GREEN}{x} + {y} = {color.END}")
     result = int(result)
-    clear()
+    
     if result == (x + y):
         correct_answer+=1
         change_question = True
         history.append(f"{color.GREEN}{x} + {y} = {result}{color.END}")
-        print_history(history=history)
         print(f"{color.GREEN}Your answer is correct!{color.END}")
     else:
         change_question = False
         history.append(f"{color.RED}{x} + {y} = {result}{color.END}")
-        print_history(history=history)
         print(f"{color.RED}Your answer is incorrect!{color.END}")
 clear()
+print_progress(done=progress, total=10)
 print_history(history=history)
 print(f"{color.GREEN}You have just finished your math. This is your score: {correct_answer * 10}{color.END}")
