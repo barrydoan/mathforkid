b6073f657f53b5159dc2875d3c1e6690cab7d326
Nhat Doan <74613870+barrydoan@users.noreply.github.com>
2023-10-17 16:37:40 -0400

    ask the same question when input wrong answer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
 correct_answer = 0
+change_question = True
+x = 0
+y = 0
 
 class color:
    PURPLE = '\033[95m'
@@ -15,14 +18,17 @@ class color:
    END = '\033[0m'
 
 for i in range(10):
-    x = random.randrange(20)
-    y = random.randrange(20)
+    if change_question:
+        x = random.randrange(20)
+        y = random.randrange(20)
     result = input(f"{color.GREEN}{x} + {y} = ")
     result = int(result)
     if result == (x + y):
         correct_answer+=1
-        print(f"Your answer is correct!{color.END}")
+        change_question = True
+        print(f"{color.GREEN}Your answer is correct!{color.END}")
     else:
+        change_question = False
         print(f"{color.RED}Your answer is incorrect!{color.END}")
 
 print(f"{color.GREEN}You have just finished your math. This is your score: {correct_answer * 10}{color.END}")
